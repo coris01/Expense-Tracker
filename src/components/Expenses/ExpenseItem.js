c900c2ca572d5af1263d1ba2594a260cca61d25f
Divyansh Kumar <k.divyansh001@gmail.com>
2022-07-01 23:20:35 +0530

    changed currency

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,7 +11,7 @@ function ExpenseItem(props) {
                 <ExpenseDate date={props.date} />
                 <div className="expense-item__description">
                     <h2>{props.title}</h2>
-                    <div className="expense-item__price">${props.amount}</div>
+                    <div className="expense-item__price">Rs.{props.amount}</div>
                 </div>
             </Card>
         </li>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,7 +7,7 @@ import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2021');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
