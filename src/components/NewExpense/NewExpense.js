349f2e6fd5c1af83a68d7b94856ca11acc892a33
Divyansh Kumar <k.divyansh001@gmail.com>
2022-07-01 22:55:37 +0530

    added the features

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(props) {
+    const [isEditing, setIsEditing] = useState(false);
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        // console.log(expenseData);
+        props.onAddExpense(expenseData);
+        setIsEditing(false);
+    }
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
+    
+    return (
+        <div className="new-expense">
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
+        </div>
+    );
+}
+
+export default NewExpense;
